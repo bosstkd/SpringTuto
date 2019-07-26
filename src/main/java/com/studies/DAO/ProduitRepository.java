855640d302619c966c8605d996def862aa42855b
Mahmoudi med Elamine <a-ek@hotmail.fr>
2019-07-26 10:53:43 +0100

    Fin du cours Partie 1 Spring MVC

--- src/main/java/com/studies/DAO/ProduitRepository.java
+++ src/main/java/com/studies/DAO/ProduitRepository.java
@@ -1,9 +1,15 @@
 package com.studies.DAO;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.studies.entities.Produit;
 
 public interface ProduitRepository extends JpaRepository<Produit, Long>{
-
+	@Query("select p from Produit p where p.designation like :mc")
+	public Page<Produit> findByDesignation(@Param("mc") String mc, Pageable pageable);
+	
 }
--- src/main/java/com/studies/controller/ProduitController.java
+++ src/main/java/com/studies/controller/ProduitController.java
@@ -3,9 +3,14 @@ package com.studies.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.studies.DAO.ProduitRepository;
 import com.studies.entities.Produit;
@@ -15,7 +20,8 @@ public class ProduitController {
 	
 	@Autowired
 	private ProduitRepository pr;
-	
+	/* 
+	 //Solution 1
 	@RequestMapping(value="/index")
 	public String index(Model model) {
 		
@@ -25,6 +31,63 @@ public class ProduitController {
 		
 		return "produits";
 	}
+	*/
+	/*
+	//Solution 2
+	@RequestMapping(value="/index")
+	public String index(Model model,
+						@RequestParam (name = "page", defaultValue = "0") int p,
+						@RequestParam (name = "size", defaultValue = "5") int s) 
+	{
+		Page<Produit> prdPage = pr.findAll(new PageRequest(p, s));
+		
+		int[] nbPages = new int[prdPage.getTotalPages()];
+		
+		model.addAttribute("listProduit", prdPage.getContent());
+		model.addAttribute("pageActuel",p);
+		model.addAttribute("size",s);
+		model.addAttribute("nbPages",nbPages);
+		return "produits";
+	}
+	*/
 	
+	@RequestMapping(value="/index")
+	public String index(Model model,
+						@RequestParam (name = "page", defaultValue = "0") int p,
+						@RequestParam (name = "size", defaultValue = "5") int s,
+						@RequestParam (name = "motCle", defaultValue = "") String mc) 
+	{
+		Page<Produit> prdPage = pr.findByDesignation("%"+mc+"%", new PageRequest(p, s));
+		
+		int[] nbPages = new int[prdPage.getTotalPages()];
+		
+		model.addAttribute("listProduit", prdPage.getContent());
+		model.addAttribute("pageActuel",p);
+		model.addAttribute("size",s);
+		model.addAttribute("nbPages",nbPages);
+		model.addAttribute("mc",mc);
+		return "produits";
+	}
+	
+	@RequestMapping(value="/delete", method =RequestMethod.GET)
+	public String delete(Model model,
+						@RequestParam (name = "id", defaultValue = "0") Long id,
+						@RequestParam (name = "page", defaultValue = "0") int p,
+						@RequestParam (name = "size", defaultValue = "5") int s,
+						@RequestParam (name = "motCle", defaultValue = "") String mc) 
+	{
+		
+		pr.delete(pr.findOne(id));
+		Page<Produit> prdPage = pr.findByDesignation("%"+mc+"%", new PageRequest(p, s));
+		
+		//int[] nbPages = new int[prdPage.getTotalPages()];
+		
+		model.addAttribute("listProduit", prdPage.getContent());
+		model.addAttribute("pageActuel",p);
+		model.addAttribute("size",s);
+		//model.addAttribute("nbPages",nbPages);
+		model.addAttribute("mc",mc);
+		return "redirect:/index?page="+p+"&size="+s+"&motCle="+mc;
+	}
 	
 }
