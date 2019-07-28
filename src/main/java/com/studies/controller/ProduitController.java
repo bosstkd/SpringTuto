7ade508b153376ebfefdf066b0e76e729c51e603
administrateur <administrateur@PC49.ccckoudiet.local>
2019-07-28 09:11:04 +0100

    test 28/07/2019

---
+++ src/main/java/com/studies/controller/ProduitController.java
@@ -0,0 +1,28 @@
+package com.studies.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.studies.DAO.ProduitRepository;
+import com.studies.entities.Produit;
+
+@Controller
+public class ProduitController {
+	
+	@Autowired
+	private ProduitRepository pr;
+	
+	
+	@RequestMapping(value="/index")
+	public String index(Model model) {
+		List<Produit> lstPrd = pr.findAll();
+		model.addAttribute("listProduit", lstPrd);
+		return "produits";
+	}
+	
+
+}
